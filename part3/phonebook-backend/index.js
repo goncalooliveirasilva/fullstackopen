a5ae2a360ff4534c272318b926c72a6e9acfa2a0
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-07-18 11:54:52 +0100

    exercise 3.22

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -13,100 +13,100 @@ app.use(express.json())
 
 
 app.get('/api/persons', (request, response, next) => {
-    Person.find({})
-        .then(persons => {
-            response.json(persons)
-        })
-        .catch(error => next(error))
+  Person.find({})
+    .then(persons => {
+      response.json(persons)
+    })
+    .catch(error => next(error))
 })
 
 
 app.get('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id
-    Person.findById(id)
-        .then(person => {
-            response.json(person)
-        })
-        .catch(error => next(error))
+  const id = request.params.id
+  Person.findById(id)
+    .then(person => {
+      response.json(person)
+    })
+    .catch(error => next(error))
 })
 
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id
-    Person.findByIdAndDelete(id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+  const id = request.params.id
+  Person.findByIdAndDelete(id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 
 app.get('/info', (request, response, next) => {
-    Person.countDocuments({})
-        .then(count => {
-            const info = `
-                <p>Phonebook has info for ${count} people</p>
-                <p>${new Date()}</p>
-            `
-            response.send(info)
-        })
-        .catch(error => next(error))
+  Person.countDocuments({})
+    .then(count => {
+      const info = `
+        <p>Phonebook has info for ${count} people</p>
+        <p>${new Date()}</p>
+      `
+      response.send(info)
+    })
+    .catch(error => next(error))
 })
 
 
 app.post('/api/persons', (request, response, next) => {
-    const {name, number} = request.body
+  const { name, number } = request.body
 
-    // if (!name) {
-    //     return response.status(400).json({
-    //         error: 'name missing'
-    //     })
-    // }
-    // if (!number) {
-    //     return response.status(400).json({
-    //         error: 'number missing'
-    //     })
-    // }
-    const person = new Person({name, number})
-    person.save()
-        .then(result => {
-            response.json(result)
-        })
-        .catch(error => next(error))
+  // if (!name) {
+  //     return response.status(400).json({
+  //         error: 'name missing'
+  //     })
+  // }
+  // if (!number) {
+  //     return response.status(400).json({
+  //         error: 'number missing'
+  //     })
+  // }
+  const person = new Person({ name, number })
+  person.save()
+    .then(result => {
+      response.json(result)
+    })
+    .catch(error => next(error))
 })
 
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const {name, number} = request.body
-    Person.findById(request.params.id)
-        .then(person => {
-            if (!person) {
-                return response.status(404).end()
-            }
-            person.name = name
-            person.number = number
-            return person.save().then(updatedPerson => {
-                response.json(updatedPerson)
-            })
-        })
-        .catch(error => next(error))
+  const { name, number } = request.body
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
+      person.name = name
+      person.number = number
+      return person.save().then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
 })
 
 
 const errorHandler = (error, request, response, next) => {
-    console.log(error.message);
-    if (error.name === 'CastError') {
-        return response.status(400).send({error: 'malformed id'})
-    }
-    if (error.name === 'ValidationError') {
-        return response.status(400).send({error: error.message})
-    }
-    next(error)
+  console.log(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformed id' })
+  }
+  if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
+  }
+  next(error)
 }
 app.use(errorHandler)
 
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 1) {
-    console.log('USAGE: <password> <name> <number>');
-    process.exit(1)
+  console.log('USAGE: <password> <name> <number>')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -14,26 +14,26 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+  name: String,
+  number: String
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
-    Person.find({}).then(result => {
-        result.forEach(entry => {
-            console.log(entry.name, entry.number);
-        })
-        mongoose.connection.close()
+  Person.find({}).then(result => {
+    result.forEach(entry => {
+      console.log(entry.name, entry.number)
     })
+    mongoose.connection.close()
+  })
 } else {
-    const name = process.argv[3]
-    const number = process.argv[4]
-    const entry = new Person({name, number})
-    entry.save().then(result => {
-        const str = `added ${result.name} ${result.number} to phonebook`
-        console.log(str);
-        mongoose.connection.close()
-    })
+  const name = process.argv[3]
+  const number = process.argv[4]
+  const entry = new Person({ name, number })
+  entry.save().then(result => {
+    const str = `added ${result.name} ${result.number} to phonebook`
+    console.log(str)
+    mongoose.connection.close()
+  })
 }
