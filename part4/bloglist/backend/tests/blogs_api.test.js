235a365049e49f7db3d3e19bc61530e453658373
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-07-24 21:48:48 +0100

    exercise 4.8

---
+++ part4/bloglist/backend/tests/blogs_api.test.js
@@ -0,0 +1,51 @@
+const { test, after, describe, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const blogs = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }
+]
+
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blog = new Blog(blogs[0])
+  await blog.save()
+  blog = new Blog(blogs[1])
+  await blog.save()
+})
+
+
+describe('testing api', () => {
+  test('returns all notes as json', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    assert.strictEqual(response.body.length, blogs.length)
+  })
+  after(async () => {
+    await mongoose.connection.close()
+  })
+})
