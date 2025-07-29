313957f36ede51f43cbf6f6695faa05fff707bcd
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-07-29 16:25:30 +0100

    exercise 4.20

--- part4/bloglist/backend/app.js
+++ part4/bloglist/backend/app.js
@@ -23,6 +23,8 @@ mongoose
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use(middleware.tokenExtractor)
+
 app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
--- part4/bloglist/backend/controllers/blogs.js
+++ part4/bloglist/backend/controllers/blogs.js
@@ -3,14 +3,6 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-const getToken = (request) => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
-
 blogsRouter.get('/',  async (request, response, next) => {
   try {
     const blogs = await Blog
@@ -29,7 +21,7 @@ blogsRouter.get('/',  async (request, response, next) => {
 blogsRouter.post('/', async (request, response, next) => {
   try {
     const body = request.body
-    const decodedToken = jwt.verify(getToken(request), process.env.SECRET)
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
     if (!decodedToken.id) {
       return response.status(401).json({ error: 'Invalid token' })
     }
