64d72cfee817e7eaacfeac089574587b5de5fe72
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-07-29 16:09:44 +0100

    exercise 4.19

--- part4/bloglist/backend/controllers/blogs.js
+++ part4/bloglist/backend/controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getToken = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/',  async (request, response, next) => {
   try {
@@ -20,9 +29,13 @@ blogsRouter.get('/',  async (request, response, next) => {
 blogsRouter.post('/', async (request, response, next) => {
   try {
     const body = request.body
-    const user = await User.findOne({})
+    const decodedToken = jwt.verify(getToken(request), process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'Invalid token' })
+    }
+    const user = await User.findById(decodedToken.id)
     if (!user) {
-      return response.status(404).json({ error: 'user not found' })
+      return response.status(404).json({ error: 'User not found' })
     }
     const blog = new Blog({
       ...body,
