6ef8838593557d3d262f7cab5c4f736b7ef89734
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-08-09 15:44:57 +0100

    exercise 5.14

--- part5/bloglist/src/components/Blog.test.jsx
+++ part5/bloglist/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders title and author by default', () => {
@@ -17,4 +18,26 @@ test('renders title and author by default', () => {
   expect(title).toHaveTextContent('title test')
   expect(author).toHaveTextContent('author test')
   expect(details).toBeNull()
+})
+
+test('renders url and likes after clicking the button', async () => {
+  const blog = {
+    title: 'title test',
+    author: 'author test',
+    url: 'www.test.com',
+    likes: 0,
+    user: { name: 'Gonçalo', username: 'goncalo' }
+  }
+  const mockHandler = vi.fn()
+  const { container } = render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('View')
+  await user.click(button)
+
+  const url = container.querySelector('.blog-url')
+  const likes = container.querySelector('.blog-likes')
+
+  expect(url).toHaveTextContent('www.test.com').toBeInTheDocument()
+  expect(likes).toHaveTextContent('0').toBeInTheDocument()
 })
