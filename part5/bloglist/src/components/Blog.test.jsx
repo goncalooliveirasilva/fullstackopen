c2816b5a0d08b6e903d462a18954c90d276a6f20
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-08-10 15:13:33 +0100

    exercise 5.15

--- part5/bloglist/src/components/Blog.test.jsx
+++ part5/bloglist/src/components/Blog.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import { userEvent } from '@testing-library/user-event'
 import Blog from './Blog'
+import blogService from '../services/blogs'
 
 test('renders title and author by default', () => {
   const blog = {
@@ -28,7 +29,7 @@ test('renders url and likes after clicking the button', async () => {
     likes: 0,
     user: { name: 'Gonçalo', username: 'goncalo' }
   }
-  const mockHandler = vi.fn()
+  // const mockHandler = vi.fn()
   const { container } = render(<Blog blog={blog} />)
 
   const user = userEvent.setup()
@@ -40,4 +41,34 @@ test('renders url and likes after clicking the button', async () => {
 
   expect(url).toHaveTextContent('www.test.com').toBeInTheDocument()
   expect(likes).toHaveTextContent('0').toBeInTheDocument()
+})
+
+test('clicking like button twice calls event handler twice', async () => {
+  const blog = {
+    title: 'title test',
+    author: 'author test',
+    url: 'www.test.com',
+    likes: 0,
+    user: { name: 'Gonçalo', username: 'goncalo' }
+  }
+
+  vi.mock('../services/blogs', () => ({
+    default: {
+      update: vi.fn()
+    }
+  }))
+
+  blogService.update.mockResolvedValue({})
+
+  render(<Blog blog={blog}/>)
+
+  const user = userEvent.setup()
+
+  const detailsButton = screen.getByRole('button', { name: 'View' })
+  await user.click(detailsButton)
+
+  await user.click(screen.getByRole('button', { name: 'Like' }))
+  await user.click(screen.getByRole('button', { name: 'Dislike' }))
+
+  expect(blogService.update).toHaveBeenCalledTimes(2)
 })
