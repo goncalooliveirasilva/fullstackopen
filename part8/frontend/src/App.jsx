369c79c03ac3c501d6947be6cbac51e508bec731
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-09-05 19:09:33 +0100

    exercise 8.11

--- part8/frontend/src/App.jsx
+++ part8/frontend/src/App.jsx
@@ -1,14 +1,21 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 
 const App = () => {
+  const [error, setError] = useState(null)
   const style = {
     paddingRight: '10px',
   }
   return (
     <BrowserRouter>
+      {error && (
+        <div style={{ color: 'red', fontSize: '30px', fontWeight: 'bold' }}>
+          {error}
+        </div>
+      )}
       <div>
         <Link style={style} to={'/'}>
           authors
@@ -21,7 +28,7 @@ const App = () => {
         </Link>
       </div>
       <Routes>
-        <Route path="/" element={<Authors />} />
+        <Route path="/" element={<Authors setError={setError} />} />
         <Route path="/books" element={<Books />} />
         <Route path="/add" element={<NewBook />} />
       </Routes>
--- part8/frontend/src/components/Authors.jsx
+++ part8/frontend/src/components/Authors.jsx
@@ -1,14 +1,34 @@
-import { ALL_AUTHORS } from '../queries'
-import { useQuery } from '@apollo/client/react'
+import { ALL_AUTHORS, UPDATE_AUTHOR } from '../queries'
+import { useQuery, useMutation } from '@apollo/client/react'
+import { useState, useEffect } from 'react'
 
-const Authors = () => {
+const Authors = ({ setError }) => {
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
   const authors = useQuery(ALL_AUTHORS)
+  const [updateAuthor, result] = useMutation(UPDATE_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  })
+
+  useEffect(() => {
+    if (result.data && result.data.editAuthor === null) {
+      setError('Author not found!')
+      setTimeout(() => setError(null), 5000)
+    }
+  }, [result.data])
 
   if (authors.loading) {
     return <div>loading...</div>
   }
 
   // console.log(authors)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    updateAuthor({ variables: { name, born: Number(born) } })
+    setName('')
+    setBorn('')
+  }
 
   return (
     <div>
@@ -29,6 +49,28 @@ const Authors = () => {
           ))}
         </tbody>
       </table>
+      <div>
+        <h3>Set birthyear</h3>
+        <form onSubmit={handleSubmit}>
+          <div>
+            name
+            <input
+              type="text"
+              value={name}
+              onChange={({ target }) => setName(target.value)}
+            />
+          </div>
+          <div>
+            born
+            <input
+              type="text"
+              value={born}
+              onChange={({ target }) => setBorn(target.value)}
+            />
+          </div>
+          <button type="submit">update auhtor</button>
+        </form>
+      </div>
     </div>
   )
 }
