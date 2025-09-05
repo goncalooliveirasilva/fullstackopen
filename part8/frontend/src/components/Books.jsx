cbfee7d95a4786bbe1d520836f90699e2c0c1965
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-09-05 18:32:16 +0100

    exercise 8.9

--- part8/frontend/src/components/Books.jsx
+++ part8/frontend/src/components/Books.jsx
@@ -1,5 +1,12 @@
-const Books = (props) => {
-  const books = []
+import { ALL_BOOKS } from '../queries'
+import { useQuery } from '@apollo/client/react'
+
+const Books = () => {
+  const books = useQuery(ALL_BOOKS)
+
+  if (books.loading) {
+    return <div>loading...</div>
+  }
 
   return (
     <div>
@@ -12,7 +19,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {books.data.allBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author}</td>
