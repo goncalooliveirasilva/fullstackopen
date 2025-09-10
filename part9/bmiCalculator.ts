2f9462da71764733d441b396fa01de4ad18d44af
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-09-10 22:33:42 +0100

    exercise 9.5

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -17,7 +17,12 @@ const parseArguments = (args: string[]): Metrics => {
   }
 };
 
-const calculateBmi = (height: number, weight: number): string => {
+export const validateArguments = (height: string, weight: string): boolean => {
+  if (!isNaN(Number(height)) && !isNaN(Number(weight))) return true;
+  return false;
+};
+
+export const calculateBmi = (height: number, weight: number): string => {
   const bmi = weight / Math.pow(height / 100, 2);
   if (bmi < 18.5) {
     return 'Underweight';
@@ -29,13 +34,15 @@ const calculateBmi = (height: number, weight: number): string => {
   return 'Obese';
 };
 
-try {
-  const { height, weight } = parseArguments(process.argv);
-  console.log(calculateBmi(height, weight));
-} catch (error: unknown) {
-  let errorMessage: string = '';
-  if (error instanceof Error) {
-    errorMessage = error.message;
+if (require.main === module) {
+  try {
+    const { height, weight } = parseArguments(process.argv);
+    console.log(calculateBmi(height, weight));
+  } catch (error: unknown) {
+    let errorMessage: string = '';
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    }
+    console.log(errorMessage);
   }
-  console.log(errorMessage);
 }
--- part9/index.ts
+++ part9/index.ts
@@ -1,9 +1,36 @@
 import express from 'express';
+import qs from 'qs';
+import { calculateBmi, validateArguments } from './bmiCalculator';
 
 const app = express();
+app.set('query parser', (str: string) =>
+  qs.parse(str, {
+    ignoreQueryPrefix: true,
+    delimiter: '&',
+    allowDots: true,
+  }),
+);
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  const { height, weight } = req.query;
+  if (
+    typeof height === 'string' &&
+    typeof weight === 'string' &&
+    validateArguments(height, weight)
+  ) {
+    const w: number = Number(weight);
+    const h: number = Number(height);
+    res.send({
+      weight: w,
+      height: h,
+      bmi: calculateBmi(h, w),
+    });
+  }
+  res.status(400).json({ error: 'malformatted parameters' });
 });
 
 const PORT = 3003;
