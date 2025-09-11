962396b4eff0db0797266a1c2a5a5b81baf3d2d2
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-09-11 17:03:23 +0100

    exercise 9.10

--- part9/backend/index.ts
+++ part9/backend/index.ts
@@ -1,20 +1,21 @@
 import express from 'express';
+import diagnosesRouter from './routes/diagnoses';
 import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+const PORT = 3001;
 
 const corsOptions = {
   origin: 'http://localhost:5173',
 };
-
 app.use(cors(corsOptions));
-
-const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
