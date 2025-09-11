97b357eefbf0b9f2ab44c86c2b1cc82ee6f081f7
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-09-11 19:00:12 +0100

    exercises 9.12-9.13

--- part9/backend/routes/patients.ts
+++ part9/backend/routes/patients.ts
@@ -1,11 +1,26 @@
 import express, { Response } from 'express';
 import patientService from '../services/patientService';
 import { NonSensitivePatient } from '../types';
+import toNewPatient from '../utils';
 
 const router = express.Router();
 
 router.get('/', (_req, res: Response<NonSensitivePatient[]>) => {
   res.send(patientService.getEntries());
+});
+
+router.post('/', (req, res) => {
+  try {
+    const newPatient = toNewPatient(req.body);
+    const addedEntry = patientService.addPatient(newPatient);
+    res.json(addedEntry);
+  } catch (error: unknown) {
+    let errorMessage = '';
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
 });
 
 export default router;
--- part9/backend/services/patientService.ts
+++ part9/backend/services/patientService.ts
@@ -1,14 +1,26 @@
-import data from '../data/patients';
-import { NonSensitivePatient } from '../types';
+import patientEntries from '../data/patients';
+import { NonSensitivePatient, NewPatient, Patient } from '../types';
+import { v1 as uuid } from 'uuid';
 
 const getEntries = (): NonSensitivePatient[] => {
-  return data.map(({ id, name, dateOfBirth, gender, occupation }) => ({
-    id,
-    name,
-    dateOfBirth,
-    gender,
-    occupation,
-  }));
+  return patientEntries.map(
+    ({ id, name, dateOfBirth, gender, occupation }) => ({
+      id,
+      name,
+      dateOfBirth,
+      gender,
+      occupation,
+    }),
+  );
 };
 
-export default { getEntries };
+const addPatient = (entry: NewPatient): Patient => {
+  const newPatient = {
+    id: uuid(),
+    ...entry,
+  };
+  patientEntries.push(newPatient);
+  return newPatient;
+};
+
+export default { getEntries, addPatient };
