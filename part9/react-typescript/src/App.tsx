98063de3e084eaf2a56a6c3df90d88ba11147a3d
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-09-13 12:19:18 +0100

    exercise 9.15

---
+++ part9/react-typescript/src/App.tsx
@@ -0,0 +1,61 @@
+interface CoursePart {
+  name: string;
+  exerciseCount: number;
+}
+
+const Header = ({ name }: { name: string }) => {
+  return <h1>{name}</h1>;
+};
+
+const Content = ({ parts }: { parts: CoursePart[] }) => {
+  return (
+    <>
+      <p>
+        {parts[0].name} {parts[0].exerciseCount}
+      </p>
+      <p>
+        {parts[1].name} {parts[1].exerciseCount}
+      </p>
+      <p>
+        {parts[2].name} {parts[2].exerciseCount}
+      </p>
+    </>
+  );
+};
+
+const Total = ({ totalExercises }: { totalExercises: number }) => {
+  return <p>Number of exercises {totalExercises}</p>;
+};
+
+const App = () => {
+  const courseName = "Half Stack application development";
+  const courseParts: CoursePart[] = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7,
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14,
+    },
+  ];
+
+  const totalExercises = courseParts.reduce(
+    (sum, part) => sum + part.exerciseCount,
+    0
+  );
+
+  return (
+    <div>
+      <Header name={courseName} />
+      <Content parts={courseParts} />
+      <Total totalExercises={totalExercises} />
+    </div>
+  );
+};
+
+export default App;
